dc6f814996b599825bc79320143e41238795ef9c
Sathiya <sathiyamohan@gmail.com>
2023-07-25 17:52:17 +0530

    sg resources added in main

--- main.tf
+++ main.tf
@@ -5,3 +5,27 @@ resource "aws_vpc" "my_vpc" {
     Name = "TF_VPC_1"
   }
 }
+
+resource "aws_security_group" "TFcustom_sg" {
+  name   = "TFcustom_sg"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
